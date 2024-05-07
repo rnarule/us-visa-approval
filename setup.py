2729e5bc6e414fc64dab8ba3e485b79fcef53603
rnarule <rnarule@gmail.com>
2024-05-07 15:50:45 +0530

    add

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="rnarule",
+    author_email="rnarule@gmail.com",
+    packages=find_packages()
+)
